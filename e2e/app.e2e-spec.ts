b62fec8cded5aa82f39a06f7d1f83031e2ea184f
Sergey Palikhov <sergeypalihov@gmail.com>
2017-01-15 18:53:13 +0300

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ChatA2Page } from './app.po';
+
+describe('chat-a2 App', function() {
+  let page: ChatA2Page;
+
+  beforeEach(() => {
+    page = new ChatA2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
